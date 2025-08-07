9b59f346e0e3b4e0165a9d03339025eedd3ded0b
Chandan Singh <chandansinghcs9536@gmail.com>
2025-08-07 23:18:53 +0530

    firstversion

---
+++ src/App.jsx
@@ -0,0 +1,25 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <h1>Hello World</h1>
+      <h2>Welcome to ERA of DevOps</h2>
+    </>
+  );
+}
+
+export default App;
+
+
+
+
+
+
+
+
+
+
+
+
+
